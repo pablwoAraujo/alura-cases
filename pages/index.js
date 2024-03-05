b3e89df59f1d30b5ec10ce001225bbed3c5d751d
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-03-05 13:55:04 -0300

    feat: estilizando o projeto

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,3 @@
-import Link from "../src/components/Link";
+import HomeScreen from '../src/screens/HomeScreen';
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura Cases - Home</h1>
-      <Link href="/faq">
-        Ir para a sessão FAQ
-      </Link>
-    </div>
-  )
-}
+export default HomeScreen;
