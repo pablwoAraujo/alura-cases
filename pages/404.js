bcb7879335bab38caed24eef4f069238d86ce19f
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-03-05 10:57:15 -0300

    feat: utilizando o componente Link

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,9 @@
 import Link from "../src/components/Link";
 
-export default function FAQPage() {
+export default function Page404() {
   return (
     <div>
-      <h1>Alura Cases - FAQ</h1>
+      <h1>Alura Cases - Página não encontrada</h1>
       <Link href="/">
         Ir para a home
       </Link>
