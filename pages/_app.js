e485d801dc6d9646a11446f8e6e87df815b8b3a2
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-03-06 13:01:15 -0300

    fix: corrigindo os scripts no head

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import GlobalStyle from '../src/theme/GlobalStyle';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
                 {/*<!-- Google tag (gtag.js) -->*/}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-88BGBPQGSK"></script>
-                <script>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-88BGBPQGSK"></Script>
+                <Script
                     dangerouslySetInnerHTML={{
                         __html: `
                             window.dataLayer = window.dataLayer || [];
@@ -17,7 +18,8 @@ function MyApp({ Component, pageProps }) {
                             gtag('config', 'G-88BGBPQGSK');
                         `
                     }}
-                </script>
+                />
+
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
